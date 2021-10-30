2b5f3b53e4de42f650928b223b7d23766b781dbf
oniranjna <oruguntaniranjan@gmail.com>
2021-10-30 20:43:25 +0530

    ==&===

---
+++ doubletriple.js
@@ -0,0 +1,6 @@
+  var x = 10;
+  console.log('Result is ', x == 10);
+  console.log('Result is ', x == "10");
+  var x = 10;
+  console.log('Result is ', x === 10);
+  console.log('Result is ', x === "10 ");
